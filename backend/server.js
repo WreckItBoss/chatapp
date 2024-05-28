3e24e1412659c8418f96777de208ad952df3d63d
WreckItBoss <isseymatsumoto0911@gmail.com>
2024-05-28 23:31:19 +0900

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-require("dotenv").config();
-const app = express();
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
+dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
     res.send("Hello World");
 })
+app.use("/api/auth", authRoutes);
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
