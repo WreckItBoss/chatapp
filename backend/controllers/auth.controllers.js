9f9fa71adc34ff08e0e977d576c8cbf770ae4753
WreckItBoss <isseymatsumoto0911@gmail.com>
2024-05-29 01:50:31 +0900

    auth

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,20 @@
+import User from "../models/user.model.js";
+
 export const signup = async(req,res) =>{
     try {
         const {fullName, username, password, confirmpassword, gender} = req.body;
+
+        if(password!=confirmPassword){
+            return res.status(400).json({error:"Password doesn't match"});
+        }
+        const user = await User.findOne({username});
+        if(user){
+            return res.status(400).json({error:"Username already exists"})
+        }
+
+        //hashing password
+        //profile pic selector https://avatar-placeholder.iran.liara.run/
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=Scott`
     } catch (error) {
         
     }
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,19 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
-dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.use("/api/auth", authRoutes);
+app.use(express.json());
 
 app.get("/", (req, res)=>{
     res.send("Hello World");
 })
-app.use("/api/auth", authRoutes);
+
 app.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
